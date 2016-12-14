173a57a50e9250b456b8cf326f04bc8550e8096a
Erik LaBianca <erik.labianca@gmail.com>
2016-12-14 15:14:07 -0500

    Bump to 0.1.5 and make bulk collections serializable

--- core/src/main/scala/com/theseventhsense/utils/collections/BulkCollection.scala
+++ core/src/main/scala/com/theseventhsense/utils/collections/BulkCollection.scala
@@ -6,7 +6,7 @@ import scala.reflect.ClassTag
   * Created by erik on 9/15/16.
   */
 
-trait BulkCollection[T] {
+trait BulkCollection[T] extends Serializable {
   def collect: Seq[T]
   def map[V](op: (T) => V)(implicit vCt: ClassTag[V]): BulkCollection[V]
   def flatMap[U](op: (T) ⇒ TraversableOnce[U])(implicit vCt: ClassTag[U]): BulkCollection[U]
@@ -15,5 +15,4 @@ trait BulkCollection[T] {
   def size: Long
   def filter(op: (T) => Boolean): BulkCollection[T]
   def count(op: (T) => Boolean): Long
-
 }
